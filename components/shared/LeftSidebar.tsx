06ceee2c7cf2545572977209bc6816e64754f73b
Surya Karan Sharma <suryakaran1234@gmail.com>
2024-03-01 18:52:09 -0500

    Working on the UI for Home-page

---
+++ components/shared/LeftSidebar.tsx
@@ -0,0 +1,5 @@
+function leftSidebar() {
+  return <h1>left Side Bar!</h1>;
+}
+
+export default leftSidebar;
---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,5 @@
+function RightSidebar() {
+    return <h1>Right Side Bar!</h1>
+}
+
+export default RightSidebar;
